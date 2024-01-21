f65332a32cfced71597af3ca0e1bddc466f030ed
Vinicius Silva <viniciusvinisilva1@outlook.com>
2024-01-21 19:21:59 -0300

    fix: wait end for finish promise download

--- src/infra/archives/fs/load-player-pair-process-download.ts
+++ src/infra/archives/fs/load-player-pair-process-download.ts
@@ -2,36 +2,38 @@ import { type LoadPlayerPairProcessDownloadRepository } from '../../../data/prot
 import fs from 'fs'
 export class LoadPlayerPairProcessDownloadFSRepository implements LoadPlayerPairProcessDownloadRepository {
   async load (download: LoadPlayerPairProcessDownloadRepository.Params): Promise<Buffer> {
-    const { id, eventEmitter, initialChunk, finalChunk } = download
-    const path = `./public/musics/${id}.mp3`
-    const exists = fs.existsSync(path)
-    if (exists) {
-      const stream = fs.createReadStream(path)
-      let totalDownloaded = 0
-      const totalSize = fs.statSync(path).size
-      const buffer: Buffer[] = []
-      let sendedFinishedChunk = false
-      stream.on('data', (chunk) => {
-        totalDownloaded += chunk.length
-        buffer.push(chunk as Buffer)
-        eventEmitter.emit('data', {
-          downloaded: totalDownloaded,
-          total: totalSize,
-          progress: Number(((totalDownloaded / totalSize) * 100).toFixed(2)),
-          buffer: Buffer.concat(buffer)
-        })
-        if (totalDownloaded >= initialChunk && totalDownloaded >= (finalChunk ?? 0) && !sendedFinishedChunk) {
-          sendedFinishedChunk = true
-          eventEmitter.emit('finished_chunk', {
-            buffer: Buffer.concat(buffer.slice(initialChunk, finalChunk))
+    return await new Promise((resolve, reject) => {
+      const { id, eventEmitter, initialChunk, finalChunk } = download
+      const path = `./public/musics/${id}.mp3`
+      const exists = fs.existsSync(path)
+      if (exists) {
+        const stream = fs.createReadStream(path)
+        let totalDownloaded = 0
+        const totalSize = fs.statSync(path).size
+        const buffer: Buffer[] = []
+        let sendedFinishedChunk = false
+        stream.on('data', (chunk) => {
+          totalDownloaded += chunk.length
+          buffer.push(chunk as Buffer)
+          eventEmitter.emit('data', {
+            downloaded: totalDownloaded,
+            total: totalSize,
+            progress: Number(((totalDownloaded / totalSize) * 100).toFixed(2)),
+            buffer: Buffer.concat(buffer)
           })
-        }
-      })
-      stream.on('end', () => {
-        eventEmitter.emit('end')
-      })
-      return Buffer.concat(buffer)
-    }
-    throw new Error('File not found')
+          if (totalDownloaded >= initialChunk && totalDownloaded >= (finalChunk ?? 0) && !sendedFinishedChunk) {
+            sendedFinishedChunk = true
+            eventEmitter.emit('finished_chunk', {
+              buffer: Buffer.concat(buffer.slice(initialChunk, finalChunk))
+            })
+          }
+        })
+        stream.on('end', () => {
+          eventEmitter.emit('end')
+          resolve(Buffer.concat(buffer))
+        })
+      }
+      throw new Error('File not found')
+    })
   }
 }
--- src/infra/youtube/download.ts
+++ src/infra/youtube/download.ts
@@ -3,39 +3,42 @@ import ytdl from 'ytdl-core'
 
 export class YoutubeDownloadRepository implements LoadPlayerPairProcessDownloadRepository {
   async load (load: LoadPlayerPairProcessDownloadRepository.Params): Promise<Buffer> {
-    const { id, eventEmitter, initialChunk, finalChunk } = load
-    const info = await ytdl.getInfo(id)
-    const audioFormats = ytdl.filterFormats(info.formats, 'audioonly')
-    if (audioFormats.length === 0) {
-      throw new Error('No audio formats found')
-    }
-    const audio = audioFormats[0]
-    const audioSize = Number(audio.contentLength)
-    const stream = ytdl.downloadFromInfo(info, {
-      format: audio
-    })
-    const buffer: Buffer[] = []
-    let totalDownloaded = 0
-    let sendedFinishedChunk = false
-    stream.on('data', (chunk) => {
-      buffer.push(chunk)
-      totalDownloaded += chunk.length as number
-      eventEmitter.emit('data', {
-        downloaded: totalDownloaded,
-        total: audioSize,
-        progress: Number(((totalDownloaded / audioSize) * 100).toFixed(2)),
-        buffer: Buffer.concat(buffer)
-      })
-      if (totalDownloaded >= initialChunk && totalDownloaded >= (finalChunk ?? 0) && !sendedFinishedChunk) {
-        sendedFinishedChunk = true
-        eventEmitter.emit('finished_chunk', {
-          buffer: Buffer.concat(buffer.slice(initialChunk, finalChunk))
-        })
+    // eslint-disable-next-line no-async-promise-executor
+    return await new Promise(async (resolve, reject) => {
+      const { id, eventEmitter, initialChunk, finalChunk } = load
+      const info = await ytdl.getInfo(id)
+      const audioFormats = ytdl.filterFormats(info.formats, 'audioonly')
+      if (audioFormats.length === 0) {
+        throw new Error('No audio formats found')
       }
+      const audio = audioFormats[0]
+      const audioSize = Number(audio.contentLength)
+      const stream = ytdl.downloadFromInfo(info, {
+        format: audio
+      })
+      const buffer: Buffer[] = []
+      let totalDownloaded = 0
+      let sendedFinishedChunk = false
+      stream.on('data', (chunk) => {
+        buffer.push(chunk)
+        totalDownloaded += chunk.length as number
+        eventEmitter.emit('data', {
+          downloaded: totalDownloaded,
+          total: audioSize,
+          progress: Number(((totalDownloaded / audioSize) * 100).toFixed(2)),
+          buffer: Buffer.concat(buffer)
+        })
+        if (totalDownloaded >= initialChunk && totalDownloaded >= (finalChunk ?? 0) && !sendedFinishedChunk) {
+          sendedFinishedChunk = true
+          eventEmitter.emit('finished_chunk', {
+            buffer: Buffer.concat(buffer.slice(initialChunk, finalChunk))
+          })
+        }
+      })
+      stream.on('end', () => {
+        eventEmitter.emit('end')
+        resolve(Buffer.concat(buffer))
+      })
     })
-    stream.on('end', () => {
-      eventEmitter.emit('end')
-    })
-    return Buffer.concat(buffer)
   }
 }
