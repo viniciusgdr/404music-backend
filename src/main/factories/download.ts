ddabb420c67ce02811e3f1d6f4bda6273fa43c6a
Vinicius Silva <viniciusvinisilva1@outlook.com>
2024-01-21 12:34:57 -0300

    chore: change to controller and usecase inject prisma and redis

--- src/main/factories/download.ts
+++ src/main/factories/download.ts
@@ -1,12 +1,12 @@
+import { type PrismaClient } from '@prisma/client'
 import { LoadPlayerPairProcessDownload } from '../../data/usecases/load-player-pair-process-download/load-player-pair-process-download'
 import { CheckLocalPlayerPairProcessDownloadFSRepository } from '../../infra/archives/fs/check-local-player-pair-process-download'
 import { LoadPlayerPairProcessDownloadFSRepository } from '../../infra/archives/fs/load-player-pair-process-download'
 import { SaveLocalPlayerPairProcessDownloadFSRepository } from '../../infra/archives/fs/save-local-player-pair-process-download'
 import { LoadMusicByIdPrismaRepository } from '../../infra/db/prisma/load-music-by-id'
 import { YoutubeDownloadRepository } from '../../infra/youtube/download'
-import { prismaClient } from '../server'
 
-export const makeDownloadUsecase = (): LoadPlayerPairProcessDownload => {
+export const makeDownloadUsecase = (prismaClient: PrismaClient): LoadPlayerPairProcessDownload => {
   const checkLocalPlayerPairProcessDownloadRepository = new CheckLocalPlayerPairProcessDownloadFSRepository()
   const loadLocalPlayerPairProcessDownloadRepository = new LoadPlayerPairProcessDownloadFSRepository()
   const loadMusicByIdRepository = new LoadMusicByIdPrismaRepository(prismaClient)
--- src/main/factories/search.ts
+++ src/main/factories/search.ts
@@ -1,14 +1,16 @@
+import { type PrismaClient } from '@prisma/client'
 import { DbSearch } from '../../data/usecases/db-search/db-search'
 import { SaveSearchPrismaRepository } from '../../infra/db/prisma/save-search-repository'
 import { RedisCacheRepository } from '../../infra/db/redis/cacheRepository'
 import { YoutubeSearchRepository } from '../../infra/youtube/search'
 import { SearchController } from '../../presentation/controllers/search/search'
 import { type Controller } from '../../presentation/protocols'
-import { RedisHelper } from '../redisClient'
-import { prismaClient } from '../server'
+import { type RedisClientType, type RedisFunctions, type RedisModules, type RedisScripts } from 'redis'
 
-export const makeSearchController = (): Controller => {
-  const cacheRepository = new RedisCacheRepository(RedisHelper.client)
+export const makeSearchController = (
+  prismaClient: PrismaClient, redisClient: RedisClientType<RedisModules, RedisFunctions, RedisScripts>
+): Controller => {
+  const cacheRepository = new RedisCacheRepository(redisClient)
   const searchRepository = new YoutubeSearchRepository()
   const saveSearchRepository = new SaveSearchPrismaRepository(prismaClient)
   const search = new DbSearch(cacheRepository, searchRepository, saveSearchRepository)
