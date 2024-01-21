19b654e4117a28565b04ae08bf712c17172f678d
Vinicius Silva <viniciusvinisilva1@outlook.com>
2024-01-21 12:24:20 -0300

    feat: implement finished_chunk event listener

--- src/infra/youtube/download.ts
+++ src/infra/youtube/download.ts
@@ -3,7 +3,7 @@ import ytdl from 'ytdl-core'
 
 export class YoutubeDownloadRepository implements LoadPlayerPairProcessDownloadRepository {
   async load (load: LoadPlayerPairProcessDownloadRepository.Params): Promise<Buffer> {
-    const { id, eventEmitter } = load
+    const { id, eventEmitter, initialChunk, finalChunk } = load
     const info = await ytdl.getInfo(id)
     const audioFormats = ytdl.filterFormats(info.formats, 'audioonly')
     if (audioFormats.length === 0) {
@@ -16,6 +16,7 @@ export class YoutubeDownloadRepository implements LoadPlayerPairProcessDownloadR
     })
     const buffer: Buffer[] = []
     let totalDownloaded = 0
+    let sendedFinishedChunk = false
     stream.on('data', (chunk) => {
       buffer.push(chunk)
       totalDownloaded += chunk.length as number
@@ -25,6 +26,12 @@ export class YoutubeDownloadRepository implements LoadPlayerPairProcessDownloadR
         progress: Number(((totalDownloaded / audioSize) * 100).toFixed(2)),
         buffer: Buffer.concat(buffer)
       })
+      if (totalDownloaded >= initialChunk && totalDownloaded >= (finalChunk ?? 0) && !sendedFinishedChunk) {
+        sendedFinishedChunk = true
+        eventEmitter.emit('finished_chunk', {
+          buffer: Buffer.concat(buffer.slice(initialChunk, finalChunk))
+        })
+      }
     })
     stream.on('end', () => {
       eventEmitter.emit('end')
