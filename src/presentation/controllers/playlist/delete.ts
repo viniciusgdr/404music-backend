3076afaf4600d097928f4749d0e6cfae4a6ff0b0
Enigma <114457825+enigma-w-e-b@users.noreply.github.com>
2024-01-24 17:31:42 -0800

    feat: add delete playlist
    * ci: update project config
    
    * feat: add delete playlist

---
+++ src/presentation/controllers/playlist/delete.ts
@@ -0,0 +1,23 @@
+import { type DeletePlaylist } from '../../../domain/usecases/delete-playlist'
+import { MissingParamError } from '../../errors'
+import { badRequest, ok, serverError } from '../../helpers/http-helper'
+import { type HttpRequest, type Controller, type HttpResponse } from '../../protocols'
+
+export class DeletePlaylistController implements Controller {
+  constructor (
+    private readonly deletePlaylist: DeletePlaylist
+  ) {}
+
+  async handle (request: HttpRequest): Promise<HttpResponse> {
+    try {
+      if (!request.body.userId) {
+        return badRequest(new MissingParamError('userId'))
+      }
+      const playlist = await this.deletePlaylist.delete(request.body)
+
+      return ok(playlist)
+    } catch (error: any) {
+      return serverError(error)
+    }
+  }
+}
