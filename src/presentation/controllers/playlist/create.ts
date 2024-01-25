de3b7a6add4416826de81c40c0ba510998611545
Vinicius Silva <viniciusvinisilva1@outlook.com>
2024-01-24 21:23:51 -0300

    feat: ensure CreatePlaylistController

---
+++ src/presentation/controllers/playlist/create.ts
@@ -0,0 +1,25 @@
+import { type CreatePlaylist } from '../../../domain/usecases/create-playlist'
+import { MissingParamError } from '../../errors'
+import { badRequest, ok, serverError } from '../../helpers/http-helper'
+import { type HttpRequest, type Controller, type HttpResponse } from '../../protocols'
+
+export class CreatePlaylistController implements Controller {
+  constructor (
+    private readonly createPlaylist: CreatePlaylist
+  ) {}
+
+  async handle (request: HttpRequest): Promise<HttpResponse> {
+    try {
+      if (!request.body.userId) {
+        return badRequest(new MissingParamError('userId'))
+      }
+      const playlist = await this.createPlaylist.create({
+        userId: request.body.userId
+      })
+
+      return ok(playlist)
+    } catch (error: any) {
+      return serverError(error)
+    }
+  }
+}
